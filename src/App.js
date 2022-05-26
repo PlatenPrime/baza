1cd048e1d6b67e5c397e19b307a01aed1ffc56e6
PlatenPrime <proshta@gmail.com>
2022-05-26 20:09:12 +0300

    Added MUI and test button and component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import Button from '@mui/material/Button';
+import SignIn from './components/SignIn';
 
 function App() {
 	return (
-		<div className='App'>Это БАЗА, детка</div>
+		<div className='App'>
+			<Button variant="contained" style={{backgroundColor: "crimson"}}>Это БАЗА, детка</Button>
+<SignIn/>
+		</div>
 	);
 }
 
